38fa88ca0196a8fb6b59f9ef37ddade2f6c253c2
NdegwaJM <juliusndegwa2@gmail.com>
2021-03-18 20:05:24 +0300

    Moving data with context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,8 +3,10 @@ import React from 'react';
 const BlogContext = React.createContext();
 
  export const BlogProvider = ({children}) => {
-    return <BlogContext.Provider>
+    return <BlogContext.Provider value={1000}>
         {children}
     </BlogContext.Provider>
 
 };
+
+export default BlogContext;
