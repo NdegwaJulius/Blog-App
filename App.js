2b2d97e4480f5b6745d36a2b462d975dda6cb0d0
NdegwaJM <juliusndegwa2@gmail.com>
2021-03-18 19:48:12 +0300

    wrapping the navigator

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import IndexScreen from './src/screens/IndexScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open  to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const navigation = createStackNavigator ({
+  Index: IndexScreen
+},{
+  initialRouteName: 'Index',
+  defaultNavigationOptions:{
+    title: 'Blogs'
+  }
 }
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App =  createAppContainer(navigation);
+export default () => {
+  return <App/>
+};
