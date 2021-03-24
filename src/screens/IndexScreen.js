6c49469cedc8efcc6d92f8084b6e01af73ef2015
NdegwaJM <juliusndegwa2@gmail.com>
2021-03-24 10:11:58 +0300

    Header navigation

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -32,6 +32,16 @@ const IndexScreen = ({navigation}) => {
     );
 };
 
+IndexScreen.navigationOptions = ({navigation}) => {
+    return {
+        headerRight:(<TouchableOpacity
+        onPress={() => navigation.navigate('Create')}
+        >
+            <Feather name="plus" size={30} color="black" />
+        </TouchableOpacity>)
+    };
+    };
+
 const styles = StyleSheet.create({
     row: {
         flexDirection:'row',
