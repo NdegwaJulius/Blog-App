598a3984ff90bbe8266a4816d76234ce6d14b631
NdegwaJM <juliusndegwa2@gmail.com>
2021-03-26 08:20:55 +0300

    making a request

---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default  axios.create ({
+    baseURL : 'http://eb850b5d6437.ngrok.io/'
+});
--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,7 +1,10 @@
+import jsonServer from '../api/jsonServer';
 import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
     switch (action.type) {
+        case'get_blogposts':
+        return action.payload;
         case 'edit_blogpost':
             return state.map(blogPost =>{
                 return blogPost.id === action.payload.id ? action.payload : blogPost;
@@ -19,6 +22,14 @@ const blogReducer = (state, action) => {
             default :
             return state;
     } 
+};
+
+const getBlogPost = dispatch => {
+    return async () => {
+     const response =  await jsonServer.get('/blogposts');
+     dispatch ({type: 'get_blogpost',payload:response.data});
+     callback();
+    };
 };
 const addBlogPost = (dispatch) => {
     return (title,content,callback) => {
@@ -49,6 +60,6 @@ const editBlogPost = (dispatch) => {
 
 export const {Context, Provider} = createDataContext(
     blogReducer, 
-    {addBlogPost , deleteBlogPost, editBlogPost},
-    [{title:'TEST POST', content: 'TEST CONTENT', id: 1}]
+    {addBlogPost , deleteBlogPost, editBlogPost,getBlogPost},
+    []
     );
