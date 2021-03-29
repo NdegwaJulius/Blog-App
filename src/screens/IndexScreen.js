8d52ca6559d4a3d1c1277947d5d0048630625cf6
NdegwaJM <juliusndegwa2@gmail.com>
2021-03-29 12:26:34 +0300

    refetching on navigate

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -8,7 +8,16 @@ const IndexScreen = ({navigation}) => {
     const {state,deleteBlogPost, getBlogPosts}= useContext(Context);
 
     useEffect (() => {
-        getBlogPosts ();
+        getBlogPosts();
+
+      const listener =  navigation.addListener ('didFocus',() =>{
+            getBlogPosts();
+        });
+         return () => {
+            listener.remove();
+
+         };
+
     }, []);
 
     return (
